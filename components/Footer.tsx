f4552bd953258924227c8311c9bfefb4488b7b9c
Satish Yadav <thesatishydv@gmail.com>
2026-01-27 16:54:54 +0530

    Satish Yadav tweet added🌵

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
       Designed and Developed by
       <Link
         className="ml-1.5 rounded-sm bg-[#E0F2FE] px-[6px] py-[1px] font-medium text-[#0369a1] transition-transform hover:scale-110 lg:px-2 lg:py-0.5 dark:bg-[#0369a1]/20 dark:text-[#E0F2FE]"
-        href="https://x.com/abdullah_twt23"
+        href="https://x.com/thesatishydv2"
         target="_black"
       >
         Satish Yadav
